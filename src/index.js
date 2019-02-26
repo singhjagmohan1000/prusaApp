18710301fe4ee9430e60d15778027d68b2349302
Jagmohan Singh <singhjagmohan4734@gmail.com>
2019-02-25 21:54:46 -0800

    basic directories and files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import App from "./App/App";
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
